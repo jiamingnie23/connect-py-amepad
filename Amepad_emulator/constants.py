05225b58d5c5adf036a81ad161eb195a20d9af17
jiaming nie <jiaming.nie@oqton.com>
2022-03-22 21:20:32 +0800

    update

---
+++ Amepad_emulator/constants.py
@@ -0,0 +1,12 @@
+import logging 
+import socket 
+import time 
+from enum import Enum 
+
+class StatusCode(Enum):
+    NotKnown = 0
+
+
+# The TCP server
+class EmulatorTCPServer(Server):
+    pass 
