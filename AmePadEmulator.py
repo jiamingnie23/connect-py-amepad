13d268b6764ebef366f17086779f0aa0187203d3
jiaming nie <jiaming.nie@oqton.com>
2022-03-24 22:46:12 +0800

    Update AmePadEmulator.py

--- AmePadEmulator.py
+++ AmePadEmulator.py
@@ -138,6 +138,190 @@ class AmePadMachine(object):
         while True:
             self.update_machine_status()
 
+#####################################################
+# The request and response configuration
+class Request(object) :
+    def __init__(self, req, action, response):
+        self.req = req
+        self.action = action
+        self.response = response
+
+    def isInterested(self, req, action) :
+        if (self.req.lower() == req.lower() and self.action.lower() == action.lower()) :
+            return True
+        return False
+
+    def GetResponse(self) :
+        return self.response
+
+class BulkRequest(object):
+    def __init__(self):
+        self.simpleRequest = {
+            "?Q100": "SERIAL NUMBER, 1234567",
+            "?Q101": "SOFTWARE VERSION, 100.16.000.1031",
+            "?Q102": "MODEL, CSMD-G2",
+            "?Q104": "MODE, ZERO",
+            "?Q200": self.toolChangesFunc,
+            "?Q201": self.toolUsedFunc,
+            "?Q300": "P.O. TIME, 06282:17:13",
+            "?Q301": "C.S. TIME, 00098:18:29",
+            "?Q303": "LAST CYCLE, 00000:00:13",
+            "?Q304": "PREV CYCLE, 00000:00:01",
+            "?Q402": "M30 #1, 380",
+            "?Q403": "M30 #2, 380",
+            "?Q500": self.programFunc
+        }
+
+    def toolChangesFunc(self):
+        return "TOOL CHANGES, " + str(random.randint(9, 15))
+
+    def toolUsedFunc(self):
+        return "USING TOOL, " + str(random.randint(1, 5))
+
+    def programFunc(self):
+        status = ['IDLE', "FEED HOLD"]
+        return "PROGRAM, MDI, %s, PARTS, %s" % (status[random.randint(0, 1)], str(random.randint(1, 50)))
+
+    def isInterested(self, req, action) :
+        if req in self.simpleRequest:
+            return True
+
+        return False
+
+    def GetResponse(self, req, action) :
+        res = self.simpleRequest[req]
+        if (callable(res)):
+            return StringResponse(res())
+        return StringResponse(res)
+
+class StringResponse(object):
+    def __init__(self, message):
+        self.msg = message
+
+    def GetResponse(self) :
+       return self.msg
+
+
+class ServerConfiguration(object) :
+    def __init__(self) :
+        self.requests = []
+
+    def AddRequest(self, request) :
+        if (request is not None) :
+            self.requests.append(request)
+
+    def ProcessRequest(self, req, action) :
+        for _req in self.requests:
+            if (_req.isInterested(req, action)) :
+                return _req.GetResponse(req, action)
+
+        return None
+    
+#######################################################
+# The TCP Server
+class EmulatorTCPServer(Server) :
+    def __init__(self, host, port, lock) :
+        self.ExitAdapterFlag = True
+        self.serverConfiguration = ServerConfiguration()
+        self.machine = MachineImpl()
+
+        # Read configuration from yaml, now just create it as Hard coded
+        self.serverConfiguration.AddRequest(BulkRequest())
+        super(EmulatorTCPServer, self).__init__(host, port, lock)
+
+    # override the listen function
+    def listen2(self, message):
+        self.sock.listen(10)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(600)
+            self.logger.debug('Client connected at: %s:%s',address[0],address[1])
+            #client.sendall(message)
+            port = address[1]
+            self.mClientList[port] = client
+            threading.Thread(target = self.communicateWithClient2,args = (client,address)).start()
+
+            self.t1 = threading.Thread(target=self.machine.run(60), args=[self])
+            self.t1.setDaemon(True)
+            self.t1.start()
+
+    def communicateWithClient2(self, client, address):
+        self.heartbeat_count = 0
+        try:
+            self.logger.debug('TCPhandle')
+            data = ""
+            buff = ""
+            heartbeats = False
+            while True:
+                try:
+                    buff = client.recv(1024)
+                except socket.timeout as e:
+                    if not heartbeats:
+                        continue
+                    else:
+                        self.logger.debug(e)
+                        client.close()
+                        return False
+
+                if not buff: break
+                with self.lock:
+                    data = data + buff
+                self.logger.debug('recv()->"%s"', buff.replace('\n','\\n'))
+
+                try:
+                    if '* PING' in data:
+                        heartbeats = True
+                        data = ""
+                        self.heartbeat_count = self.heartbeat_count + 1
+                        buff = '* PONG 30000\n'
+                        self.logger.debug(str(self.heartbeat_count) + " " + buff.replace('\n', '\\n'))
+                        client.sendall(buff)
+                    else :
+                        if (self.ProcessUnknownData(client, data)) :
+                            data = ''
+                except:
+                    pass
+
+        except Exception as e:
+            self.logger.debug(e)
+            client.close()
+            return False
+
+    def ProcessUnknownData(self, client, data):
+        if ('\n' in data) :
+            message = data
+            self.logger.debug("EmulatorTCPServer::DealWithUnknownData")
+
+            try :
+                # Deal with request, remove the \r, \n and space
+                message = message.replace('\n', '').replace('\r', '').replace(' ', '')
+                messages = message.split(',')
+                if ((messages is not None) and (len(messages) >= 1)) :
+                    if len(messages) > 1:
+                        action = messages[1]
+                    else:
+                        action = None
+                    response = self.serverConfiguration.ProcessRequest(messages[0], action)
+                    if (response is not None) :
+                        responseMsg = str(response.GetResponse())
+                        #print responseMsg
+                        client.sendall(responseMsg + '\n')
+                    else:
+                        client.sendall("?," + messages[0] + '\n')
+                    return True
+
+                # Wow, unknonw data
+                client.sendall('PLC:Alive\n')
+            except BaseException as e:
+                self.logger.debug("EmulatorTCPServer::Process data met exception: " + e)
+            finally :
+                return True
+
+        # Not end input, let parent deal with it
+        return False
+
+    def close(self) :
+        pass
 
 if __name__ == "__main__":
     time_in_sec = 2500
